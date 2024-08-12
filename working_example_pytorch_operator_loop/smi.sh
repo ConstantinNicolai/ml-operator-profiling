78e598be70487242acf995ad7508e7bba1f880fc
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-08-12 15:28:06 +0200

    issueing the default run 10 times in a row

--- working_example_pytorch_operator_loop/smi.sh
+++ working_example_pytorch_operator_loop/smi.sh
@@ -25,42 +25,42 @@ kill_background_jobs() {
 
 
 # Define lists for each parameter
-in_channels_list=(32 64 128)
-out_channels_list=(64 128 256)
-kernel_size_list=(3 5 7)
-stride_list=(1 1 1)
-padding_list=(1 1 1)
-batch_size_list=(16 32 64)
-ifmap_size_list=(28 32 56)
+in_channels_list=(64 64 64 64 64 64 64 64 64 64)
+out_channels_list=(128 128 128 128 128 128 128 128 128 128)
+kernel_size_list=(3 3 3 3 3 3 3 3 3 3)
+stride_list=(1 1 1 1 1 1 1 1 1 1 )
+padding_list=(1 1 1 1 1 1 1 1 1 1 )
+batch_size_list=(32 32 32 32 32 32 32 32 32 32)
+ifmap_size_list=(56 56 56 56 56 56 56 56 56 56)
 
 
 
-# num_combinations=${#in_channels_list[@]}
-# # Run the benchmark for each indexed combination of parameters
-# for ((i=0; i<$num_combinations; i++)); do
-#     in_channels=${in_channels_list[$i]}
-#     out_channels=${out_channels_list[$i]}
-#     kernel_size=${kernel_size_list[$i]}
-#     stride=${stride_list[$i]}
-#     padding=${padding_list[$i]}
-#     batch_size=${batch_size_list[$i]}
-#     ifmap_size=${ifmap_size_list[$i]}
+num_combinations=${#in_channels_list[@]}
+# Run the benchmark for each indexed combination of parameters
+for ((i=0; i<$num_combinations; i++)); do
+    in_channels=${in_channels_list[$i]}
+    out_channels=${out_channels_list[$i]}
+    kernel_size=${kernel_size_list[$i]}
+    stride=${stride_list[$i]}
+    padding=${padding_list[$i]}
+    batch_size=${batch_size_list[$i]}
+    ifmap_size=${ifmap_size_list[$i]}
 
-#     echo "Running: in_channels=$in_channels, out_channels=$out_channels, kernel_size=$kernel_size, stride=$stride, padding=$padding, batch_size=$batch_size, ifmap_size=$ifmap_size"
-#     srun python3 attempt_4.py \
-#         --in_channels "$in_channels" \
-#         --out_channels "$out_channels" \
-#         --kernel_size "$kernel_size" \
-#         --stride "$stride" \
-#         --padding "$padding" \
-#         --batch_size "$batch_size" \
-#         --ifmap_size "$ifmap_size" \
-#         >> logs/training_output_${SLURM_JOB_ID}.log
-# done
+    echo "Running: in_channels=$in_channels, out_channels=$out_channels, kernel_size=$kernel_size, stride=$stride, padding=$padding, batch_size=$batch_size, ifmap_size=$ifmap_size"
+    srun python3 attempt_4.py \
+        --in_channels "$in_channels" \
+        --out_channels "$out_channels" \
+        --kernel_size "$kernel_size" \
+        --stride "$stride" \
+        --padding "$padding" \
+        --batch_size "$batch_size" \
+        --ifmap_size "$ifmap_size" \
+        >> logs/training_output_${SLURM_JOB_ID}.log
+done
 
 
 
-# Run the benchmark
-srun python3 attempt_4.py --in_channels 128 --out_channels 256 --kernel_size 7 --stride 1 --padding 1 --batch_size 64 --ifmap_size 56 >> logs/training_output_${SLURM_JOB_ID}.log
+# # Run the benchmark
+# srun python3 attempt_4.py --in_channels 128 --out_channels 256 --kernel_size 7 --stride 1 --padding 1 --batch_size 64 --ifmap_size 56 >> logs/training_output_${SLURM_JOB_ID}.log
 
 # srun python3 attempt_4.py --ifmap_size 32 >> logs/training_output_${SLURM_JOB_ID}.log
