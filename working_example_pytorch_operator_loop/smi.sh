9388854e4f786af814f0993b9a287383e3f1b13b
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-08-14 10:24:18 +0200

    more parameter changes

--- working_example_pytorch_operator_loop/smi.sh
+++ working_example_pytorch_operator_loop/smi.sh
@@ -25,13 +25,13 @@ kill_background_jobs() {
 
 
 # Define lists for each parameter
-in_channels_list=(3 64 64 64 256 256 128 128 64 64)
-out_channels_list=(64 64 64 256 64 128 128 512 128 128)
-kernel_size_list=(7 1 3 1 1 1 3 1 3 3)
-stride_list=(2 1 1 1 1 1 2 1 1 1 )
-padding_list=(3 0 1 0 0 0 1 0 1 1 )
-batch_size_list=(32 32 32 32 32 32 32 32 32 32)
-ifmap_size_list=(224 56 56 56 56 56 56 56 56 56)
+in_channels_list=(3 64 64 64 256 256 128 128)
+out_channels_list=(64 64 64 256 64 128 128 512)
+kernel_size_list=(7 1 3 1 1 1 3 1)
+stride_list=(2 1 1 1 1 1 2 1)
+padding_list=(3 0 1 0 0 0 1 0)
+batch_size_list=(32 32 32 32 32 32 32 32)
+ifmap_size_list=(224 56 56 56 56 56 56 56)
 
 
 
