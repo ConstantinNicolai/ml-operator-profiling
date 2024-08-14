5a4e817e0d8b0038ecffddcad2f4acb03447a711
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-08-14 13:08:57 +0200

    default wieghts

--- profiling_playground/resnet.py
+++ profiling_playground/resnet.py
@@ -1,14 +1,14 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision.models import resnet18
+from torchvision.models import resnet18, ResNet18_Weights
 from torchvision import transforms
 from torchsummary import summary
 from torch_profiling_utils.fvcorewriter import FVCoreWriter
 from torch_profiling_utils.torchinfowriter import TorchinfoWriter
 
 # Load the pretrained ResNet-50 model
-model = resnet18(pretrained=True)
+model = resnet18(weights=ResNet18_Weights.DEFAULT)
 
 
 
