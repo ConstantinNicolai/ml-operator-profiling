34641dcc0aed49247da602762b3cfb09c86eace0
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-08-14 16:56:18 +0200

    another one

--- profiling_playground/model_overview.sh
+++ profiling_playground/model_overview.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# load appropriate conda paths, because we are not in a login shell
+eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+conda activate constabass
+
 operator="conv" 
 
 python3 print.py | grep $operator >> temp_print_$operator
