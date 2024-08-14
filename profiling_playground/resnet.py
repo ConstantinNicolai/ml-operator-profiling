16898305bbbf5c690eec1f9d3696283e0de044de
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-08-14 13:01:21 +0200

    back to 18 found obvious blunder

--- profiling_playground/resnet.py
+++ profiling_playground/resnet.py
@@ -1,14 +1,15 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision.models import resnet50
+from torchvision.models import resnet18, ResNet18_Weights
+
 from torchvision import transforms
 from torchsummary import summary
 from torch_profiling_utils.fvcorewriter import FVCoreWriter
 from torch_profiling_utils.torchinfowriter import TorchinfoWriter
 
 # Load the pretrained ResNet-50 model
-model = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
+model = resnet18(weights=ResNet18_Weights.IMAGENET1K_V2)
 
 
 
