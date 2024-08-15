967554f0ec77607448c8ad03f9fbbe5d9e8c7a8c
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-08-15 14:33:22 +0200

    step by step

--- profiling_playground/model_overview.sh
+++ profiling_playground/model_overview.sh
@@ -6,18 +6,14 @@ conda activate constabass
 
 operator="conv"
 
-python3 print.py | grep $operator >> temp_print_$operator
+python3 print.py | grep $operator > temp_print_$operator
 
-python3 tree.py | grep -o "$operator.*" >> temp_tree_$operator
+python3 tree.py | grep -o "$operator.*" > temp_tree_$operator
 
-sleep 25s
+# python3 dictex_unique_count.py temp_print_$operator
 
-python3 dictex_unique_count.py temp_print_$operator
+# python3 treestudy1.py temp_tree_$operator
 
-python3 treestudy1.py temp_tree_$operator
-
-sleep 10s
-
-python3 merge_dicts.py temp_print_$operator.json temp_tree_$operator.json resnet34_$operator
+# python3 merge_dicts.py temp_print_$operator.json temp_tree_$operator.json resnet34_$operator
 
 
