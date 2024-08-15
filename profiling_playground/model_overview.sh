1553a8574a1ddb91e190e44446db8af922fb0b0c
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-08-15 14:11:41 +0200

    added some sleep statements into the pipeline as barriers

--- profiling_playground/model_overview.sh
+++ profiling_playground/model_overview.sh
@@ -10,9 +10,13 @@ python3 print.py | grep $operator >> temp_print_$operator
 
 python3 tree.py | grep -o "$operator.*" >> temp_tree_$operator
 
+sleep(25)
+
 python3 dictex_unique_count.py temp_print_$operator
 
 python3 treestudy1.py temp_tree_$operator
+
+sleep(10)
 
 python3 merge_dicts.py temp_print_$operator.json temp_tree_$operator.json resnet34_$operator
 
