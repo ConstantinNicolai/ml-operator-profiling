05e18ef6821dffb518793332dd358f052dd92a21
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-08-15 14:51:20 +0200

    changed naming

---
+++ operator_search/operator_search.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# load appropriate conda paths, because we are not in a login shell
+eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+conda activate constabass
+
+operator="conv"
+
+python3 model_analysis_print.py | grep $operator > temp_print_$operator
+
+python3 model_analysis_bigtree.py | grep -o "$operator.*" > temp_tree_$operator
+
+python3 filter_print_results.py temp_print_$operator
+
+python3 filter_bigtree_results.py temp_tree_$operator
+
+python3 merge_dicts.py temp_print_$operator.json temp_tree_$operator.json resnet34_$operator
