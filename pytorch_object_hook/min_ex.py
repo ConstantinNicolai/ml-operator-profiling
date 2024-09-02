1896958a22497a45933e0036739910e0d9d7e8f2
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-09-02 18:57:48 +0200

    ignore min_ex, just learnign basics of object oreiented stuff, but follow readme intructions

--- pytorch_object_hook/min_ex.py
+++ pytorch_object_hook/min_ex.py
@@ -3,7 +3,10 @@ from torchvision import models, transforms
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision.models import resnet34, ResNet34_Weights
+from torchvision.models import maxvit_t, MaxVit_T_Weights
+from torchvision.models import inception_v3, Inception_V3_Weights
 from dataclasses import dataclass
+import numpy as np
 
 
 @dataclass
@@ -112,7 +115,121 @@ print(layer_info1)
 
 print(list(list(model.layer1.children())[0].named_children())[0][1])
 
+
+print('############################################################')
+
+
+print(dir(list(model.named_children())[0]))
+
 # depending on the model architecture there wil be a lot to iterate over
 
 # in this case we need the direct sublayers of model, and then the names layers like layer1
 # their children, which have named_children, and those are tuples of the name and the pytorch object
+
+print('############################################################')
+
+model = maxvit_t(weights=MaxVit_T_Weights.DEFAULT).to(device)
+
+
+# print(dir(model))
+
+# maxvir = str(dir(model))
+
+# print(dir(list(model.named_children())[0][1]))
+
+# print(list(model.children())[0])
+
+# print(dir(list(list(model.children())[0].children())[0]))
+
+# print(list(model.children())[1][2][1])
+
+# print(list(list(model.children())[1].children())[2])
+
+
+# for i in range(len(list(model.children()))):
+#     print(i)
+#     for j in range(len(list(list(model.children())[i].children()))):
+#         print(j)
+#         for k in range(len())
+
+
+
+gustave = list(model.children())
+
+# print(gustave[1][0])
+
+
+# print(test)
+
+# print(gustave)
+
+print(len(gustave))
+
+
+tt= 0
+for i in range(len(gustave)):
+    ttnew = len(gustave[i])
+    if ttnew > tt:
+        tt = ttnew
+
+print(tt)
+
+tb= 0
+for i in range(len(gustave)):
+    for k in range(tt):
+        try:
+            tbnew = len(gustave[i][k])
+            if tbnew > tb:
+                tb = tbnew
+        except Exception as e:
+            continue
+
+print(tb)
+
+
+
+tc= 0
+for i in range(len(gustave)):
+    for k in range(tt):
+        for j in range(tb):
+            try:
+                tcnew = len(gustave[i][k][j])
+                if tcnew > tc:
+                    tc = tbnew
+            except Exception as e:
+                continue
+
+
+print(tc)
+
+# for i in range(len(gustave)):
+#     for k in range(tt):
+#         try:
+#             for j in range(tb):
+#                 try:
+#                     print(gustave[i][k][j])
+#                 except Exception as e:
+#                     continue
+#         except Exception as e:
+#             continue
+
+
+
+# print(len(gustave)[:][:])
+
+# for i in range(4):
+#     print('t000est')
+
+print('############################################################')
+
+model = inception_v3(weights=Inception_V3_Weights.DEFAULT).to(device)
+
+print(model)
+
+# print(dir(model))
+
+# inception = str(dir(model))
+
+# assert inception == inception, "they're not the same"
+
+
