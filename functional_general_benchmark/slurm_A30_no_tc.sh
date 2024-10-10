6d1317d450c01366f3ff784f35756e31177bee6d
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-10-10 12:45:40 +0200

    adding scripts for A30 runs without tensor cores

---
+++ functional_general_benchmark/slurm_A30_no_tc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --partition=all
+#SBATCH --job-name=operator_profiling
+#SBATCH --output=operator_profiling_A30_no_tc.out
+#SBATCH --error=operator_profiling_A30_no_tc.er
+#SBATCH --nodes=1 
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:A30:1
+
+
+
+# load appropriate conda paths, because we are not in a login shell
+eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+conda activate constabass
+
+
+# kill_background_jobs() {
+#     for pid in $@; do
+#         kill $pid
+#     done
+# }
+
+python3 general_pipeline_block2_A30_no_tc.py
