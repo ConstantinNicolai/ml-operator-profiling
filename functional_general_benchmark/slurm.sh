2d3ad7d1f63d9a032705ab4d33e65e1b4d1a3363
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2025-04-08 13:21:40 +0200

    started validation runs for fullmodel inference

--- functional_general_benchmark/slurm.sh
+++ functional_general_benchmark/slurm.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --partition=rivulet
 #SBATCH --nodes=1 
-#SBATCH --job-name=A30vm210
-#SBATCH --output=A30vm210.out
+#SBATCH --job-name=A30vm1440
+#SBATCH --output=A30vm1440.out
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:A30:1
 #SBATCH --time=168:00:00
@@ -17,14 +17,14 @@ conda activate constabass
 
 
 
-sudo /opt/csg/scripts/nvidia-set-clocks.sh -d 0 -c 210 &
+sudo /opt/csg/scripts/nvidia-set-clocks.sh -d 0 -c 1440 &
 CLOCK_PID=$!;
 printf "setting clock in the backround at PID %s\n" $CLOCK_PID
 sleep 5;
 echo "Query clock to confirm"
 nvidia-smi --query-gpu=clocks.gr,clocks.mem --format=csv;
 
-python3 full_model_meas_infvalidation.py  --gpu=A30_210 --rundur=14 --runnr=30
+python3 full_model_meas_infvalidation.py  --gpu=A30_1440 --rundur=14 --runnr=20
 # python3 full_model_meas_train.py --gpu=A30_210 --rundur=12 --runnr=60
 # python3 general_pipeline_block2_backprop.py --gpu=A30_210 --rundur=10 --runnr=25
 
